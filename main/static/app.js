16dafd92ab40cee4e763c31c6ad9091ce7faf4c2
MaciejMM <maciej.myslinski88@gmail.com>
2020-04-27 22:41:30 +0200

    Project setup
    Project setup including all files and structure

---
+++ main/static/app.js
@@ -0,0 +1,35 @@
+let divContainer = document.querySelectorAll('.container div');
+
+window.addEventListener('load', (e) => {
+    const preload = document.querySelector('.loader-wrapper');
+    preload.classList.add('preload-finish')
+})
+
+let firstImgs = divContainer[0].querySelectorAll('img');
+let hidedElements = divContainer[0].querySelectorAll('.hide-img')
+
+
+let counter = 0;
+function countToThree(){
+
+  firstImgs.forEach((e)=>{
+    e.classList.add('hide-img')
+
+  })    
+
+
+          firstImgs[counter].classList.remove('hide-img')
+          firstImgs[counter].addEventListener('transitionend',()=>{
+            firstImgs[counter].classList.add('show-img')
+
+          })
+          // firstImgs[counter].classList.add('show-img')
+          // s
+
+        counter++
+        if(counter===firstImgs.length){
+            counter = 0
+        }
+}
+
+setInterval(countToThree,3000)
